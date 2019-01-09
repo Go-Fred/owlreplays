3a1b5bd104340a97fc063d303e39494ad586960c
Frederick Duberville <frederick.duberville@gmail.com>
2019-01-09 08:56:58 -0500

    Test to push to Github

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -5,7 +5,7 @@ export default class Error extends Component {
   render() {
       return (
         <div className="error" >
-          <p>¯\_(ツ)_/¯</p><br></br><p>Unable to retrieve videos from Twitch. Please retry in a couple of minutes</p>
+          <p>¯\_(ツ)_/¯</p><br></br><p>Unable to retrieve videos from Twitch. Please retry in a couple of minutes.</p>
         </div>
       );
   }
